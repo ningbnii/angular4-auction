8ad9f3fb77bf24393adb6ce5244e83dd3129dadc
yesixuan ye <yesixuan ye>
2017-06-04 13:27:52 +0800

    '评论组件,onChanges()钩子的使用，组件之间的双向绑定'

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -10,6 +10,9 @@ import { ProductService,Product,Comment } from '../shared/product.service'
 export class ProductDetailComponent implements OnInit {
 	product: Product
 	comments: Comment[]
+  newRating: number = 5
+  newComment: string = ''
+  isCommentHidden: boolean = true
   constructor(
   	private routeInfo: ActivatedRoute,
   	private productService: ProductService
@@ -20,5 +23,14 @@ export class ProductDetailComponent implements OnInit {
   	this.product = this.productService.getProuduct(productId)
   	this.comments = this.productService.getCommentsForProductId(productId)
   }
-
+  addComment() {
+    let comment = new Comment(0,this.product.id,new Date().toISOString(),'小水',this.newRating,this.newComment)
+    this.comments.unshift(comment)
+    this.newComment = null
+    this.newRating = 5
+    this.isCommentHidden = true
+    // 第一次调那个匿名函数时，传进去的是0。返回的值会作为下一次的sum传进去
+    let sum = this.comments.reduce((sum,comment) => sum + comment.rating,0)
+    this.product.rating = sum / this.comments.length
+  }
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,21 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, OnChanges, EventEmitter, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
 	@Input() // 这样整一下，父组件传过来的同名属性自然就同步过来了
 	private rating: number = 0;
 	private stars: boolean[];
+  // 控制星星组件是否只读
+  @Input()
+  private readonly: boolean = true;
+  // 把新的评分给父组件
+  @Output()
+  // 只有在输出输的变量后面加Change才能直接用双向绑定，否则要另写方法
+  private ratingChange: EventEmitter<number> = new EventEmitter()
   constructor() { }
 
   ngOnInit() {
-  	this.stars = [];
-  	for(let i = 1; i <= 5; i++) {
-  		this.stars.push(i > this.rating? true: false) // 其实这里的三元可以不写，直接写条件
-  	}
+  	
   }
-
+  clickStar(index: number) { // 传进来的下标是从0开始的
+    if(!this.readonly) {
+      this.rating = index+1;
+      this.ngOnInit() // ngOnInit还可以这样调，学着点儿
+    }
+    this.ratingChange.emit(this.rating)
+  }
+  // 这个接口用来解决父组件中的评分已经改变，但是传到子组件中没有变化的问题
+  ngOnChanges(changes: SimpleChanges): void {
+    this.stars = [];
+    for(let i = 1; i <= 5; i++) {
+      this.stars.push(i > this.rating? true: false) // 其实这里的三元可以不写，直接写条件
+    }
+  }
 }
