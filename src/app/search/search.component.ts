2056730be7baa63968fef2bf884601f2a6d7c49f
yesixuan ye <yesixuan ye>
2017-06-05 20:37:17 +0800

    '响应式表单'

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms'
+import { ProductService } from '../shared/product.service'
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+	formModel: FormGroup
+	categories: string[]
+  constructor(fb: FormBuilder, private productService: ProductService) {
+  	this.formModel = fb.group({
+  		title: ['',[Validators.minLength(3)]],
+  		price: [null,[this.positiveNumberValidator]],
+  		category: ['-1']
+  	})
   }
 
+  ngOnInit() {
+  	this.categories = this.productService.getAllCategories()
+  }
+  // 定义只能输入正数的规则
+  positiveNumberValidator(control: FormControl):any {
+  	if(!control.value) {
+  		return null
+  	}
+  	let myReg = /^[0-9]*[1-9][0-9]*$/
+  	if(myReg.test(control.value)) {
+  		return null
+  	}
+  	return {positiveNumber: true}
+  }
+  onSearch() {
+  	if(this.formModel.valid) {
+  		console.log(this.formModel.value)
+  	}
+  }
 }
--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -3,12 +3,12 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProductService {
  	private products: Product[] = [
-		new Product(1,"第1个商品",1.99,3.5,"这是一个商品的描述1",["电子产品1","通讯设备1"]),
-		new Product(2,"第2个商品",1.58,4.5,"这是一个商品的描述2",["电子产品2","通讯设备2"]),
-		new Product(3,"第3个商品",1.52,2.5,"这是一个商品的描述3",["电子产品3","通讯设备3"]),
-		new Product(4,"第4个商品",1.45,1,"这是一个商品的描述4",["电子产品4","通讯设备4"]),
-		new Product(5,"第5个商品",1.6,2.5,"这是一个商品的描述5",["电子产品5","通讯设备5"]),
-		new Product(6,"第6个商品",1.85,4,"这是一个商品的描述6",["电子产品6","通讯设备6"]),
+		new Product(1,"第1个商品",1.99,3.5,"这是一个商品的描述1",["电子产品","通讯设备"]),
+		new Product(2,"第2个商品",1.58,4.5,"这是一个商品的描述2",["图书","通讯设备"]),
+		new Product(3,"第3个商品",1.52,2.5,"这是一个商品的描述3",["图书","通讯设备"]),
+		new Product(4,"第4个商品",1.45,1,"这是一个商品的描述4",["电子产品","通讯设备"]),
+		new Product(5,"第5个商品",1.6,2.5,"这是一个商品的描述5",["电子产品","通讯设备"]),
+		new Product(6,"第6个商品",1.85,4,"这是一个商品的描述6",["电子产品","通讯设备"]),
 	]
 	private comments: Comment[] = [
 		new Comment(1,1,"2017-05-28 14:14:14","巴金",3,"为你写诗"),
@@ -17,6 +17,9 @@ export class ProductService {
 		new Comment(4,4,"2017-01-28 14:14:55","小骚球",3,"为你写诗")
 	]
   constructor() { }
+  getAllCategories(): string[] {
+  	return ['电子产品','通讯设备','图书']
+  }
   // 得到商品列表
 	getProuducts(): Product[] {
 		return this.products
